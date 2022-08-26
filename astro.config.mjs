01eedd5a2081d4d95634021b07b817877107fd42
Federico Bozo <federico@priisma.com>
2022-08-26 15:59:27 -0400

    Add Astro Image

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,14 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
-
 import tailwind from "@astrojs/tailwind";
+
+import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [preact(), tailwind({
-    // Example: Disable injecting a basic `base.css` import on every page.
-    // Useful if you need to define and/or import your own custom `base.css`.
-    config: { applyBaseStyles: false },
-  })]
+    config: {
+      applyBaseStyles: false
+    }
+  }), image()]
 });
