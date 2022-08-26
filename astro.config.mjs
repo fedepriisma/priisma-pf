8937081eec0d46cdedab5a21e3131e395229e9ff
Federico Bozo <federico@priisma.com>
2022-08-26 16:08:04 -0400

    Config for deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -6,6 +6,7 @@ import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://fedepriisma.github.io',
   integrations: [preact(), tailwind({
     config: {
       applyBaseStyles: false
