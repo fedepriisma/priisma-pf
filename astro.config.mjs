b22d4989148362ef8219e1ac5b50da393904b09c
Federico Bozo <federico@priisma.com>
2022-09-08 18:29:10 -0400

    Added GA, partytown and some content

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,15 +1,24 @@
 import { defineConfig } from 'astro/config';
 import preact from '@astrojs/preact';
 import tailwind from "@astrojs/tailwind";
-
+import partytown from "@astrojs/partytown";
 import image from "@astrojs/image";
 
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.priisma.com',
-  integrations: [preact(), tailwind({
+  integrations: [
+    preact(), tailwind({
     config: {
       applyBaseStyles: false
     }
-  }), image()]
+    }),
+    image(),
+    partytown({
+      // Adds dataLayer.push as a forwarding-event.
+      config: {
+        forward: ["dataLayer.push"],
+      },
+    }),
+  ]
 });
