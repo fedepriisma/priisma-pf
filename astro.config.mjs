16aa6f7f1563860919e7156b66e4284ae5eb7ef0
Fede Bozo <fede@cigarwell.com>
2026-03-04 13:20:02 -0500

    Refactor typography styles across SWAT SEO pages and update privacy policy and terms of service for consistency
    - Increased font sizes from base to md for improved readability in index.astro
    - Updated headings in privacy-policy.astro and terms-and-conditions.astro to use md font size
    - Adjusted text classes in various sections to maintain uniformity
    - Enhanced tailwind CSS variables for better color and typography management
    - Introduced new font "Cabinet Grotesk" for improved aesthetics

--- astro.config.mjs
+++ astro.config.mjs
@@ -2,17 +2,19 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from "@tailwindcss/vite";
 import partytown from '@astrojs/partytown';
 
+import sitemap from '@astrojs/sitemap';
+
 // https://astro.build/config
 export default defineConfig({
   site: 'https://www.priisma.com',
-  integrations: [
-    partytown({
-      // Adds dataLayer.push as a forwarding-event.
-      config: {
-        forward: ["dataLayer.push"],
-      },
-    }),
-  ],
+  integrations: [partytown({
+    // Adds dataLayer.push as a forwarding-event.
+    config: {
+      forward: ["dataLayer.push"],
+    },
+  }), sitemap({
+    filter: (page) => !page.includes('/footer/'),
+  })],
   vite: {
     plugins: [tailwindcss()],
     build: {
